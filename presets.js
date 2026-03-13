2ac734f831b7d4b1b8f83e72d501a75ff42995eb
meekste10 <96758212+meekste10@users.noreply.github.com>
2026-03-13 15:22:01 -0500

    Initial QR Camo engine upload
    https://meekste10.github.io/QR-Camo

---
+++ presets.js
@@ -0,0 +1,11 @@
+
+export const maskPresets = {
+heart:"assets/masks/heart.png",
+house:"assets/masks/house.png",
+tag:"assets/masks/tag.png",
+star:"assets/masks/star.png",
+pizza:"assets/masks/pizza.png",
+mug:"assets/masks/mug.png",
+tree:"assets/masks/tree.png",
+turtle:"assets/masks/turtle.png"
+}
---
+++ state.js
@@ -0,0 +1,12 @@
+
+export const state = {
+qrImage:null,
+qrImageData:null,
+thresholded:null,
+cropped:null,
+tileLibrary:[],
+maskImage:null,
+cropPercent:22,
+tileSize:14,
+safeCore:true
+}
