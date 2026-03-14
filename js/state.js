902fda767650427d0d55864e2f2fb78027dc65b6
meekste10 <96758212+meekste10@users.noreply.github.com>
2026-03-14 15:48:43 -0500

    Format state object for consistency

--- js/state.js
+++ js/state.js
@@ -1,12 +1,11 @@
-
 export const state = {
-qrImage:null,
-qrImageData:null,
-thresholded:null,
-cropped:null,
-tileLibrary:[],
-maskImage:null,
-cropPercent:22,
-tileSize:14,
-safeCore:true
-}
+  qrImage: null,
+  qrImageData: null,
+  thresholded: null,
+  cropped: null,
+  tileLibrary: [],
+  maskImage: null,
+  cropPercent: 22,
+  tileSize: 14,
+  safeCore: true
+};
