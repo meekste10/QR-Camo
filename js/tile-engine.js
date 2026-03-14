9148640c22cf067f62b2940a1b000efca26b14d8
meekste10 <96758212+meekste10@users.noreply.github.com>
2026-03-14 15:52:01 -0500

    Refactor extractTiles function for clarity and efficiency

--- js/tile-engine.js
+++ js/tile-engine.js
@@ -1,13 +1,28 @@
+export function extractTiles(imageData, tileSize) {
+  const tiles = [];
 
-export function extractTiles(imageData,tileSize){
-const tiles=[]
-for(let y=0;y<imageData.height;y+=tileSize){
-for(let x=0;x<imageData.width;x+=tileSize){
-const canvas=new OffscreenCanvas(tileSize,tileSize)
-const ctx=canvas.getContext("2d")
-ctx.putImageData(imageData,-x,-y)
-tiles.push(canvas)
-}
-}
-return tiles
+  const sourceCanvas = document.createElement("canvas");
+  sourceCanvas.width = imageData.width;
+  sourceCanvas.height = imageData.height;
+  const sctx = sourceCanvas.getContext("2d");
+  sctx.putImageData(imageData, 0, 0);
+
+  for (let y = 0; y < imageData.height; y += tileSize) {
+    for (let x = 0; x < imageData.width; x += tileSize) {
+      const tileCanvas = document.createElement("canvas");
+      tileCanvas.width = tileSize;
+      tileCanvas.height = tileSize;
+      const tctx = tileCanvas.getContext("2d");
+
+      tctx.drawImage(
+        sourceCanvas,
+        x, y, tileSize, tileSize,
+        0, 0, tileSize, tileSize
+      );
+
+      tiles.push(tileCanvas);
+    }
+  }
+
+  return tiles;
 }
