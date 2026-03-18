a5be2e648f98293c1ff18004119c616d71bd6eca
meekste10 <96758212+meekste10@users.noreply.github.com>
2026-03-18 00:29:22 -0500

    Update export.js

--- js/export.js
+++ js/export.js
@@ -1,5 +1,6 @@
-import { downloadCanvas } from "./image-utils.js";
-
-export function exportPNG(canvas) {
-  downloadCanvas(canvas);
+export function exportPNG(canvas, filename = "qr-camo.png") {
+  const link = document.createElement("a");
+  link.download = filename;
+  link.href = canvas.toDataURL("image/png");
+  link.click();
 }
