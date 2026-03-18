60aa390cc150e8f3c3a8e2005746aefce6262164
meekste10 <96758212+meekste10@users.noreply.github.com>
2026-03-18 00:32:02 -0500

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,11 +1,6 @@
 export const state = {
   qrImage: null,
   qrImageData: null,
-  thresholded: null,
-  cropped: null,
-  tileLibrary: [],
-  maskImage: null,
-  safeCore: true,
   customMaskImage: null,
   customMaskCanvas: null
 };
