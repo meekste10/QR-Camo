213c7549f394e22295be4b4d0d0c0b36f243e0b7
meekste10 <96758212+meekste10@users.noreply.github.com>
2026-03-19 23:16:03 -0500

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,11 +1,17 @@
 export const state = {
   qrImage: null,
   qrImageData: null,
+
   customMaskImage: null,
   customMaskCanvas: null,
+
   sourceQrCanvas: null,
   overlayQrCanvas: null,
+  normalizedQrCanvas: null,
+  interiorCanvas: null,
+
   textureTiles: [],
+
   moduleCount: 21,
   modulePixelSize: 1
 };
