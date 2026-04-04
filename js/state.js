9e8739e8d89589f9adf842009e8b0f5ed72e1cc5
meekste10 <96758212+meekste10@users.noreply.github.com>
2026-04-04 11:23:46 -0500

    Update state.js

--- js/state.js
+++ js/state.js
@@ -13,8 +13,31 @@ export const state = {
   textureTiles: [],
 
   moduleCount: 21,
-  modulePixelSize: 1
+  modulePixelSize: 1,
+
+  hasRenderedOnce: false,
+
+  selectedChannelId: 1,
+  defaultChannelSize: "xxsmall",
+
+  qrChannels: [
+    { id: 1, enabled: true, size: "xxsmall", x: 0, y: 0, autoX: 0, autoY: 0, fitScore: 0, cornersFit: 0, overlapRisk: 0 },
+    { id: 2, enabled: true, size: "xxsmall", x: 0, y: 0, autoX: 0, autoY: 0, fitScore: 0, cornersFit: 0, overlapRisk: 0 },
+    { id: 3, enabled: true, size: "xxsmall", x: 0, y: 0, autoX: 0, autoY: 0, fitScore: 0, cornersFit: 0, overlapRisk: 0 },
+    { id: 4, enabled: true, size: "xxsmall", x: 0, y: 0, autoX: 0, autoY: 0, fitScore: 0, cornersFit: 0, overlapRisk: 0 },
+    { id: 5, enabled: true, size: "xxsmall", x: 0, y: 0, autoX: 0, autoY: 0, fitScore: 0, cornersFit: 0, overlapRisk: 0 }
+  ],
+
+  placementCandidates: [],
+
+  stapledBaseCanvas: null,
+  currentMaskCanvas: null,
+  lastBaseSignature: null,
+
+  channelOverlayCanvases: {},
+  compositeCanvas: null
 };
+
 state.baseCanvas = null;
 state.baseCtx = null;
 
