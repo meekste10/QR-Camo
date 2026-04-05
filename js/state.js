fd7b07d2ffde79cb2f4fec6934c324b92fcce9f9
meekste10 <96758212+meekste10@users.noreply.github.com>
2026-04-04 23:32:27 -0500

    Update state.js

--- js/state.js
+++ js/state.js
@@ -18,35 +18,10 @@ export const state = {
 
   hasRenderedOnce: false,
 
-  selectedChannelId: 1,
-  defaultChannelSize: "xxsmall",
-  liveQrScale: 1,
-  channelsDirty: true,
-
-  qrChannels: [
-    { id: 1, enabled: true, size: "xxsmall", x: 0, y: 0, autoX: 0, autoY: 0, fitScore: 0, cornersFit: 0, overlapRisk: 0, qrDisplaySize: 0, moduleDisplaySize: 0 },
-    { id: 2, enabled: true, size: "xxsmall", x: 0, y: 0, autoX: 0, autoY: 0, fitScore: 0, cornersFit: 0, overlapRisk: 0, qrDisplaySize: 0, moduleDisplaySize: 0 },
-    { id: 3, enabled: true, size: "xxsmall", x: 0, y: 0, autoX: 0, autoY: 0, fitScore: 0, cornersFit: 0, overlapRisk: 0, qrDisplaySize: 0, moduleDisplaySize: 0 }
-  ],
-
-  placementCandidates: [],
-
   stapledBaseCanvas: null,
   currentMaskCanvas: null,
   lastBaseSignature: null,
 
-  channelOverlayCanvases: {},
-  compositeCanvas: null,
-
-  baseCanvas: null,
-  baseCtx: null,
-
-  liveTransform: {
-    x: 0,
-    y: 0,
-    scale: 1,
-    isDragging: false
-  },
-
-  isShowingSample: false
+  qrPlacement: null,
+  liveQrScale: 1
 };
